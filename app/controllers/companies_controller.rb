bc7c2c233723542138e205a2cc6951b73c403845
Anup Baidya <anupbaidya619@gmail.com>
2021-08-20 23:29:30 +0530

    Revert "y"
    This reverts commit f0f4b398f74297634c7e162a2715e203e364ccd5.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,7 +2,6 @@ class CompaniesController < ApplicationController
   before_action :set_company, only: [:show, :edit, :update, :destroy]
 
 #this is to be revver first
-  #this is revert second
   # GET /companies
   # GET /companies.json
   def index
