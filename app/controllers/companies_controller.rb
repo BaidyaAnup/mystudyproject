a88a95656ac0de5593a7c1d04e385515c3fb66bf
Anup Baidya <anupbaidya619@gmail.com>
2021-08-20 23:19:59 +0530

    fix con

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,6 @@
 class CompaniesController < ApplicationController
   before_action :set_company, only: [:show, :edit, :update, :destroy]
+
 #this is to be revver first
   #this is revert second
   # GET /companies
