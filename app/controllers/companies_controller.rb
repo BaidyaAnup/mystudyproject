722dbae43185625991902915b5ddd1ac52452ba5
Anup Baidya <anupbaidya619@gmail.com>
2021-08-20 23:26:19 +0530

    Revert "ffl"
    This reverts commit 6225e56f2fe423c7767933a28b639452cbc42b8d.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,8 @@
 class CompaniesController < ApplicationController
   before_action :set_company, only: [:show, :edit, :update, :destroy]
 
+#this is to be revver first
+  #this is revert second
   # GET /companies
   # GET /companies.json
   def index
